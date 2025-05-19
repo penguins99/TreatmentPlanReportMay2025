140f0172bff065f330372b53dd2e0221b8e4c46e
GSStudentB1 <GSStudentB1@GSSTUDENTB-TBOX>
2025-05-19 12:56:35 -0400

    Add project files.

---
+++ TreatmentPlanReport/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using TreatmentPlanReport.ViewModels;
+using TreatmentPlanReport.Views;
+
+
+//Reference ESAPI libraries
+//Reference with Alias beacuase the Application class is also in System.Windows
+using esapi = VMS.TPS.Common.Model.API;
+
+namespace TreatmentPlanReport
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            using (esapi.Application app = esapi.Application.CreateApplication())
+            {
+                //TODO: Get Patient Info from Context.
+                esapi.Patient patient = app.OpenPatientById("RapidPlan-02");//hard code patient for now.
+                Window window = new Window();
+                PatientView patientView = new PatientView();//using TreatmentPlanReport.Views;
+                patientView.DataContext = new PatientViewModel(patient);//using TreatmentPlanReport.ViewModels;
+                window.Content = patientView;// app.CurrentUser.Name;
+                window.ShowDialog();//pause the app here.
+                //window.Show();//continue the app (then the app object is disposed).
+            }
+        }
+    }
+}
---
+++ TreatmentPlanReport/ViewModels/PatientViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.TPS.Common.Model.API;
+
+namespace TreatmentPlanReport.ViewModels
+{
+    //make classoublic
+    public class PatientViewModel
+    {
+        public string PatientId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Hospital { get; set; }
+
+        public string DateOfBirth { get; set; }
+
+        //Constructor can pass in arguments as well as set properties.
+
+        public PatientViewModel(Patient patient)
+        {
+            PatientId = patient.Id;
+            Name = $"{patient.LastName}, {patient.FirstName}";//string Interpolation!!
+            Hospital = patient.Hospital.Id;
+            DateOfBirth = patient.DateOfBirth == null ?//ternary operator
+                "No DOB" ://true part.
+                patient.DateOfBirth.Value.ToShortDateString();//false part.
+        }
+    }
+}
