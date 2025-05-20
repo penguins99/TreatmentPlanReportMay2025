ceb1189ecc703ade9a210d9b628de36efb70c58a
GSStudentB1 <GSStudentB1@GSSTUDENTB-TBOX>
2025-05-20 15:21:37 -0400

    Add OxyPlot support and implement DVH functionality
    Updated `TreatmentPlanReport.csproj` to include OxyPlot library references and added new source files for event handling and DVH (Dose Volume Histogram) functionality. Introduced `EventHelper` and `ViewModelBase` classes for event-driven programming and property change notifications. Modified `MainViewModel` to include `DVHViewModel` and `DVHSelectionViewModel`. Enhanced `MainView.xaml` with new tabs for DVH selection and display. Created `DVHSelectionView` and `DVHView` user controls for UI interaction. Implemented structure selection management in `StructureSelectionModel` and `DVHSelectionViewModel`, and improved `DVHViewModel` for plot generation. Added `packages.config` for OxyPlot dependency management.

--- TreatmentPlanReport/ViewModels/MainViewModel.cs
+++ TreatmentPlanReport/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DoseMetricExample.Helpers;
+using DVHPlot.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,11 +16,16 @@ namespace TreatmentPlanReport.ViewModels
             PatientViewModel = new PatientViewModel(patient);
             PlanViewModel = new PlanViewModel(plan);
             FieldViewModel = new FieldViewModel(plan);
+            EventHelper eventHelper = new EventHelper();
+            DVHViewModel = new DVHViewModel(plan, eventHelper);
+            DVHSelectionViewModel = new DVHSelectionViewModel(plan, eventHelper);
         }
 
         //The spelling of these properties must match the biding expressions in the MainView.xaml file.
         public PatientViewModel PatientViewModel { get; }
         public PlanViewModel PlanViewModel { get; }
         public FieldViewModel FieldViewModel { get; }
+        public DVHViewModel DVHViewModel { get; }
+        public DVHSelectionViewModel DVHSelectionViewModel { get; }
     }
 }
